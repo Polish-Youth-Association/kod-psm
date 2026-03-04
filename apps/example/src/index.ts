76d83cb17c00700210fb70fe8e721cd196f5ea03
MarcinB <134430645+MarcinBi@users.noreply.github.com>
2026-03-04 17:15:44 -0500

    Update index.ts

--- apps/example/src/index.ts
+++ apps/example/src/index.ts
@@ -3,7 +3,7 @@ import type { RequestHandler } from "express";
 
 const PORT = Number(process.env.PORT) || 8080;
 
-const app = express();
+const app = express();  //comment
 
 const handler: RequestHandler = (_req, res) => {
   res.json({ ok: true, service: "PING!" });
