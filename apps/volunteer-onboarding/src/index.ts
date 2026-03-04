46d4756a3e28a3a1f300100b70e69a651a6e36d0
michalbienias-psm <michal.bienias@polishyouth.org>
2026-03-04 17:08:03 -0500

    added todo

--- apps/volunteer-onboarding/src/index.ts
+++ apps/volunteer-onboarding/src/index.ts
@@ -23,6 +23,8 @@ function sleep(ms: number) {
   return new Promise<void>((resolve) => setTimeout(resolve, ms));
 }
 
+// TODO: Implement Jobs and Tasks //
+
 function newId() {
   return `req_${Math.random().toString(36).slice(2, 10)}`;
 }
