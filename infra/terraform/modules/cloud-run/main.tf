0761fb10e6dbc5a8fd3f279ed4a8885ef1d1a29b
michalbienias-psm <michal.bienias@polishyouth.org>
2026-03-30 20:36:05 -0400

    fix: preserve cpu_idle setting in cloud-run module

--- infra/terraform/modules/cloud-run/main.tf
+++ infra/terraform/modules/cloud-run/main.tf
@@ -23,6 +23,7 @@ resource "google_cloud_run_v2_service" "service" {
           memory = var.memory
         }
         startup_cpu_boost = true
+        cpu_idle          = true
       }
 
       dynamic "env" {
